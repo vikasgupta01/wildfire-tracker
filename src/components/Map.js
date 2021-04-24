0e89fa4e7762ef62f39efd2e4925d75875e523e3
vikasgupta01 <vikasgupta2552@gmail.com>
2021-04-24 21:42:48 +0530

    code upto adding a marker to custom locations using google maps and iconify api

---
+++ src/components/Map.js
@@ -0,0 +1,31 @@
+import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
+
+const Map = ({ center, zoom }) => {
+    return (
+        <div className="map">
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key:
+                        'AIzaSyCpfbijN8XM96GuNz0ElUvxBDK-TfEbAUA'
+                }}
+                defaultCenter={center}
+                defaultZoom={zoom}
+            >
+                <LocationMarker lat={center.lat} lng={center.lng} />
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+Map.defaultProps = {
+    center: {
+        // lat: 42.3265,
+        // lng: -122.8756
+        lat: 24.3782,
+        lng: 81.3042
+    },
+    zoom: 6
+}
+
+export default Map
