c0640a6d45add6209414430e4a2eed688d4a4b06
vikasgupta01 <vikasgupta2552@gmail.com>
2021-04-25 09:08:53 +0530

    add wildfire icon on every active wildfire location in map using NASA API

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,15 @@
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
 
-const Map = ({ center, zoom }) => {
+const Map = ({ eventData, center, zoom }) => {
+
+    const markers = eventData.map(ev => {
+        if (ev.categories[0].id === 8) {
+            return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+        }
+        return null
+    })
+
     return (
         <div className="map">
             <GoogleMapReact
@@ -12,7 +20,7 @@ const Map = ({ center, zoom }) => {
                 defaultCenter={center}
                 defaultZoom={zoom}
             >
-                <LocationMarker lat={center.lat} lng={center.lng} />
+                {markers}
             </GoogleMapReact>
         </div>
     )
@@ -20,10 +28,10 @@ const Map = ({ center, zoom }) => {
 
 Map.defaultProps = {
     center: {
-        // lat: 42.3265,
-        // lng: -122.8756
-        lat: 24.3782,
-        lng: 81.3042
+        lat: 42.3265,
+        lng: -122.8756
+        // lat: 24.3782,
+        // lng: 81.3042
     },
     zoom: 6
 }
