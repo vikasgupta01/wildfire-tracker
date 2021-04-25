458b3e5a10c98d53173d0dfc7a91c99d0abb0663
vikasgupta01 <vikasgupta2552@gmail.com>
2021-04-25 11:00:29 +0530

    add header and style locationInfoBox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Map from './components/Map'
 import Loader from './components/Loader'
+import Header from './components/Header'
 
 function App() {
   const [eventData, setEventData] = useState([])
@@ -23,6 +24,7 @@ function App() {
 
   return (
     <div>
+      < Header />
       { !loading ? <Map eventData={eventData} /> : <Loader />}
       {/* <Map eventData={eventData} /> */}
     </div>
